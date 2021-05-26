d3692c7c4113836617046777e421533afab7b7ff
Carlos Guagrilla <carlos.guagrilla@hotmail.com>
2021-05-26 11:04:45 -0500

    tema

--- front-page.php
+++ front-page.php
@@ -1,2 +1,13 @@
 <?php get_header(); ?> 
+<main>
+    <div class="w-1/12">
+        <img src="<?php echo get_template_directory_uri() ?>/assert/img/1.jpg" 
+        alt="imagen de perfil del tema">
+    </div>
+    <div class="w-6/12">
+        <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus totam quaerat id, labore odit, itaque architecto vero laboriosam assumenda quas temporibus recusandae provident doloribus qui exercitationem error ipsum corrupti molestias.
+        </p>
+    </div>
+</main>
 <?php get_footer(); ?> 
