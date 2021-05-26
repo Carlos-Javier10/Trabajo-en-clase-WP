b05b55304101755d69e27ba458b51f61b3c285ae
Carlos Guagrilla <carlos.guagrilla@hotmail.com>
2021-05-26 11:53:36 -0500

    tema

--- front-page.php
+++ front-page.php
@@ -1,7 +1,12 @@
 <?php get_header(); ?> 
 <main>
+<body style="background-color:#2980B9;">
+<h1 align="center">
+Presentacion
+</h1>
     <div class="w-1/12">
-        <img src="<?php echo get_template_directory_uri() ?>/assets/img/1.jpg" 
+        <img style="border-radius: 20px;"
+        src="<?php echo get_template_directory_uri() ?>/assets/img/1.jpg" 
         alt="imagen de perfil del tema"
         width="190" height="190">
     </div>
@@ -10,5 +15,22 @@
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus totam quaerat id, labore odit, itaque architecto vero laboriosam assumenda quas temporibus recusandae provident doloribus qui exercitationem error ipsum corrupti molestias.
         </p>
     </div>
+    <br>
+    <div>
+    <p>
+    <img style="border-radius: 20px;" 
+        src="<?php echo get_template_directory_uri() ?>/assets/img/5.jpg" 
+        alt="imagen de perfil del tema"
+        width="190" height="190"
+        align="left"><br><br>
+        
+        
+    Me llamo Carlos y he probramado mayormente en Java y Php puesto, que ya he tenido un poco de experiencia laboral usando esos dos lenguajes.<br>
+    He trabajado ayudando a mis superiores en mejor un programa de una de empresa el cual llevaba los registros de los productos, haciamos la <br> 
+    implementacion de nuevas secciones al programa. Ademas de eso tambien trabajamos con la base de datos haciendo unas pequeñas modificaciones <br>
+    con todo eso usamos ambos lenguajes de programacion.
+    </p>
+    </div>
 </main>
+</body>
 <?php get_footer(); ?> 
